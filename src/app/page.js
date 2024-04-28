aee8cb70166f72fe1b6ad6b2afaa2afb29dc88e8
Yeming Hu <yeminghuhym@gmail.com>
2024-04-28 19:57:45 -0400

    HomeMenu and About section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,34 @@
 import Header from "@/components/layout/Header";
 import Hero from "@/components/layout/Hero";
+import HomeMenu from "@/components/layout/HomeMenu";
+import SectionHeaders from "@/components/layout/SectionHeaders";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <HomeMenu />
+      <section className="text-center my-16">
+        <SectionHeaders subHeader={"Our story"} mainHeader={"About us"} />
+        <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do.
+          </p>
+        </div>
+      </section>
     </>
   );
 }
